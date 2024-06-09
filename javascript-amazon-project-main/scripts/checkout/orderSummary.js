099344fb481385b6663e24c9c63c2e2bccbbf54e
PremKumar <premkumar020405@gmail.com>
2024-06-10 00:58:46 +0530

    create automated tests for order summary

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -30,7 +30,7 @@ export function renderOrderSummary(){
 
       
       `
-      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+      <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: ${dateString}
       </div>
@@ -46,7 +46,7 @@ export function renderOrderSummary(){
           <div class="product-price">
             $${formatCurrency(matchingProduct.priceCents)}
           </div>
-          <div class="product-quantity">
+          <div class="product-quantity js-product-quantity-${matchingProduct.id}">
             <span>
               Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
             </span>
@@ -56,7 +56,7 @@ export function renderOrderSummary(){
             </span>
             <input class="quantity-input js-quantity-input-${matchingProduct.id}">
             <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id="${matchingProduct.id}">Save</span>
-            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+            <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
@@ -115,9 +115,10 @@ export function renderOrderSummary(){
         removeFromCart(productId);
 
         renderOrderSummary();
+        renderPaymentSummary();
         renderCheckoutHeader();
 
-        renderPaymentSummary();
+        
       });
     });
 
