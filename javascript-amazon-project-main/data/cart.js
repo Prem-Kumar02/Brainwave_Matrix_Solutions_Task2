babcd79572917d53626246d3bc8d7d8ab1e40b07
PremKumar <premkumar020405@gmail.com>
2024-06-09 23:57:05 +0530

    create automated tests for add to cart

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,8 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
+loadFromStorage();
+
+export function loadFromStorage(){
+   cart = JSON.parse(localStorage.getItem('cart'));
 
 if(!cart){
   cart= [{
@@ -10,6 +14,8 @@ if(!cart){
     quantity:1,
     deliveryOptionId:'2'
   }];
+}
+
 }
 
 
